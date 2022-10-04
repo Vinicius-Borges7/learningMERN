d4b20fad41fa36c23e3409e2a68ba880842cc959
Vinicius-Borges7 <vinicius.shr@gmail.com>
2022-10-04 08:58:48 -0300

    zxc zxmdzx.jndsmln

--- teste06 - ejs/main.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.set('view engine', 'ejs');
-app.get('/home', (req, res) => {
-    res.render('../views/home');
-});
-
-app.listen(1234, () => {console.log("o servidor está rodando!")});
